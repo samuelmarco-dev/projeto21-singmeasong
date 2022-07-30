22d9c31bffae7869b2dc21ad13ef3bca0b928896
Samuel Marco <samuelmarco.2020@alunos.utfpr.edu.br>
2022-07-30 11:44:19 -0300

    docs: updating settings for both backend and frontend

--- sing-me-a-song-main/back-end/src/app.ts
+++ sing-me-a-song-main/back-end/src/app.ts
@@ -1,12 +1,15 @@
-import cors from "cors";
-import express from "express";
+import express, { json } from "express";
 import "express-async-errors";
+import cors from "cors";
+import morgan from "morgan";
+
 import { errorHandlerMiddleware } from "./middlewares/errorHandlerMiddleware.js";
 import recommendationRouter from "./routers/recommendationRouter.js";
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(json());
+app.use(morgan("dev"));
 
 app.use("/recommendations", recommendationRouter);
 app.use(errorHandlerMiddleware);
---
+++ sing-me-a-song-main/back-end/tests/app.test.ts
@@ -0,0 +1,7 @@
+import supertest from "supertest";
+
+import app from "../src/app.js";
+
+describe('teste', ()=> {
+    
+});
