3eca77ab537db03a48b5d46c9353a72409286d9e
Samuel Marco <samuelmarco.2020@alunos.utfpr.edu.br>
2022-07-31 17:35:57 -0300

    feat: unit tests when creating a recommendation and duplicate validation; creating upvote and downvote on a recommendation

--- sing-me-a-song-main/back-end/tests/unit/recommendationService.test.ts
+++ sing-me-a-song-main/back-end/tests/unit/recommendationService.test.ts
@@ -1,30 +1,29 @@
-import { faker } from "@faker-js/faker";
 import { jest } from "@jest/globals";
 
+import recommendationFactory from "../factories/recommendationFactory.js";
 import { recommendationRepository } from '../../src/repositories/recommendationRepository.js';
 import { CreateRecommendationData, recommendationService } from "../../src/services/recommendationsService.js";
 
+beforeEach(() => {
+    jest.clearAllMocks();
+    jest.resetAllMocks();
+});
+
 describe('recommendationsService test suite', ()=> {
     it('should create a recommendation', async ()=> {
+        const recomendation: CreateRecommendationData = recommendationFactory.generateRecommendation();
+        
         jest.spyOn(recommendationRepository, 'findByName').mockImplementationOnce(():any => {});
         jest.spyOn(recommendationRepository, 'create').mockResolvedValueOnce(null);
 
-        const recomendation: CreateRecommendationData = {
-            name: 'test',
-            youtubeLink: `https://www.youtube.com/watch?v=${faker.lorem.word(11)}`
-        };
         await recommendationService.insert(recomendation);
-
         expect(recommendationRepository.findByName).toBeCalled();
         expect(recommendationRepository.create).toBeCalled();
     });
 
     it('should not create a recommendation if it already exists', async ()=> {
-        const recomendation: CreateRecommendationData = {
-            name: 'test',
-            youtubeLink: `https://www.youtube.com/watch?v=${faker.lorem.word(11)}`
-        };
-
+        const recomendation: CreateRecommendationData = recommendationFactory.generateRecommendation();
+        
         jest.spyOn(recommendationRepository, 'findByName').mockImplementationOnce(():any => {
             return {
                 id: 1,
@@ -33,14 +32,86 @@ describe('recommendationsService test suite', ()=> {
                 score: 0
             }
         });
-        jest.spyOn(recommendationRepository, 'create').mockResolvedValueOnce(null);
         
-        const promise = await recommendationService.insert(recomendation);
-        expect(recommendationRepository.findByName).toBeCalled();
-        expect(recommendationRepository.create).not.toBeCalled();
+        const promise = recommendationService.insert(recomendation);
         expect(promise).rejects.toEqual({ message: 'Recommendations names must be unique', type: 'conflict' });
+        expect(recommendationRepository.findByName).toBeCalled();
+    });
+    
+    it('should create upvote a recommendation', async ()=> {
+        const recomendation: CreateRecommendationData = recommendationFactory.generateRecommendation();
+        
+        jest.spyOn(recommendationRepository, 'find').mockImplementationOnce(():any => {
+            return {
+                id: 1,
+                name: recomendation.name,
+                youtubeLink: recomendation.youtubeLink,
+                score: 0
+            }
+        });
+        jest.spyOn(recommendationRepository, 'updateScore').mockImplementationOnce(():any => {
+            return {
+                id: 1,
+                name: recomendation.name,
+                youtubeLink: recomendation.youtubeLink,
+                score: 1
+            }
+        });
+
+        await recommendationService.upvote(1);
+        expect(recommendationRepository.find).toBeCalled();
+        expect(recommendationRepository.updateScore).toBeCalled();
+    });
+    
+    it('should create downvote a recommendation', async ()=> {
+        const recomendation: CreateRecommendationData = recommendationFactory.generateRecommendation();
+        
+        jest.spyOn(recommendationRepository, 'find').mockImplementationOnce(():any => {
+            return {
+                id: 1,
+                name: recomendation.name,
+                youtubeLink: recomendation.youtubeLink,
+                score: 0
+            }
+        });
+        jest.spyOn(recommendationRepository, 'updateScore').mockImplementationOnce(():any => {
+            return {
+                id: 1,
+                name: recomendation.name,
+                youtubeLink: recomendation.youtubeLink,
+                score: -1
+            }
+        });
+
+        await recommendationService.downvote(1);
+        expect(recommendationRepository.find).toBeCalled();
+        expect(recommendationRepository.updateScore).toBeCalled();
     });
 
-    it('should create upvote a recommendation', async ()=> {});
-    it('should create downvote a recommendation', async ()=> {});
+    it('should not create downvote a recommendation if it has a score less than -5', async ()=> {
+        const recomendation: CreateRecommendationData = recommendationFactory.generateRecommendation();
+        
+        jest.spyOn(recommendationRepository, 'find').mockImplementationOnce(():any => {
+            return {
+                id: 1,
+                name: recomendation.name,
+                youtubeLink: recomendation.youtubeLink,
+                score: -5
+            }
+        });
+        jest.spyOn(recommendationRepository, 'updateScore').mockImplementationOnce(():any => {
+            return {
+                id: 1,
+                name: recomendation.name,
+                youtubeLink: recomendation.youtubeLink,
+                score: -6
+            }
+        });
+        jest.spyOn(recommendationRepository, 'remove').mockResolvedValueOnce(null);
+
+        await recommendationService.downvote(1);
+        expect(recommendationRepository.find).toBeCalled();
+        expect(recommendationRepository.updateScore).toBeCalled();
+        expect(recommendationRepository.remove).toBeCalled();
+    });
 });
