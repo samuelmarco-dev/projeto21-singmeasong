3704f3626b84a266fdf8b4aba107ac7b294ced95
Samuel Marco <samuelmarco.2020@alunos.utfpr.edu.br>
2022-08-01 11:23:47 -0300

    feat: test e2e for positive or negative vote request

--- sing-me-a-song-main/front-end/cypress/e2e/appTest.cy.js
+++ sing-me-a-song-main/front-end/cypress/e2e/appTest.cy.js
@@ -3,18 +3,17 @@ import { faker } from "@faker-js/faker";
 
 const URL = "http://localhost:3000";
 const RECOMMENDATIONS_NUMBERS = 0;
+const recommendation = {
+    name: faker.lorem.words(),
+    youtubeLink: `https://www.youtube.com/watch?v=${faker.lorem.word(11)}`
+};
 
 before(() => {
     cy.resetRecommendations();
 });
 
 describe('app test', ()=> {
-    it('create a new recommendation', ()=> {
-        const recommendation = {
-            name: faker.lorem.words(),
-            youtubeLink: `https://www.youtube.com/watch?v=${faker.lorem.word(11)}`
-        };
-
+    it('create a new recommendation, given valid input', ()=> {
         cy.visit(`${URL}/`);
         cy.get('article').should('have.length', RECOMMENDATIONS_NUMBERS);
 
@@ -26,7 +25,42 @@ describe('app test', ()=> {
         cy.wait('@createRecommendation');
 
         cy.url().should("equal", `${URL}/`);
+        cy.contains(recommendation.name);
         cy.get('article').should('have.length', (RECOMMENDATIONS_NUMBERS + 1));
+    });
+
+    it('create create upvote a recommendation', ()=> {
+        cy.visit(`${URL}/`);
+        
+        cy.get('article').should('have.length', (RECOMMENDATIONS_NUMBERS + 1));
+        cy.get('article').within(() => {
+            cy.get("div:last-of-type").should("have.text", "0");
+        });
+        cy.contains(recommendation.name).get("article").within(() => {
+            cy.get("svg:first-of-type").click();
+        });
+
+        cy.reload(true);
+        cy.contains(recommendation.name).get("article").within(() => {
+            cy.get("div:last-of-type").should("have.text", "1");
+        });
+    });
+
+    it('create downvote a recommendation', ()=> {
+        cy.visit(`${URL}/`);
+
+        cy.get('article').should('have.length', (RECOMMENDATIONS_NUMBERS + 1));
+        cy.get('article').within(() => {
+            cy.get("div:last-of-type").should("have.text", "1");
+        });
+        cy.contains(recommendation.name).get("article").within(() => {
+            cy.get("svg:last-of-type").click();
+        });
+
+        cy.reload(true);
+        cy.contains(recommendation.name).get("article").within(() => {
+            cy.get("div:last-of-type").should("have.text", "0");
+        });
     });
 
     it('find the last 10 recommendations', ()=> {
